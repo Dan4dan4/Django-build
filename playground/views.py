25888874839545d02bcafdd49932141deb5b123e
Dan4dan4 <danm44@hotmail.com>
2025-02-28 18:53:50 -0500

    create a function in views.py

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# Here we create a function that takes a request and gives response also known as a request handler
+
+def say_hello(request):
+    # in this function we can pull data from db, transform stuff, send emails etc
+    return HttpResponse('Hello World')
