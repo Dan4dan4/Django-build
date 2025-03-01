e0e95575d4978b967105e5a3f67d2f646b6b89b0
Dan4dan4 <danm44@hotmail.com>
2025-02-28 19:51:32 -0500

    changed the harcode to atemplate html

--- playground/views.py
+++ playground/views.py
@@ -7,4 +7,10 @@ from django.http import HttpResponse
 
 def say_hello(request):
     # in this function we can pull data from db, transform stuff, send emails etc
-    return HttpResponse('Hello World')
+    # return HttpResponse('Hello World')
+
+    # this is going to show the file hello.html which i created in templates
+    # return render(request, 'hello.html')
+
+    #now im adding a dictionary to hard/soft code it
+    return render(request, 'hello.html', { 'name': 'Danish'})
